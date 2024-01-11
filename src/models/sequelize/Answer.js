ca1fa5b2107b9a9551dd2be3b8ea7bbcc5e4e19b
sourisooo <119761249+sourisooo@users.noreply.github.com>
2024-01-11 17:09:27 +0100

    Create Answer.js

---
+++ src/models/sequelize/Answer.js
@@ -0,0 +1,16 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("./sequelize-client");
+
+class Answer extends Model {}
+
+Answer.init({
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  tableName: "answer"
+});
+
+module.exports = Answer;
